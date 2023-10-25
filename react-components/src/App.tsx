a83ee82fcdded68c8a97cc01548f5cebcc19e31c
FreightDH <maxim.zolotkov@yandex.ru>
2023-10-25 13:46:07 +0300

    init: start task react-components

---
+++ react-components/src/App.tsx
@@ -0,0 +1,15 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <>
+      <main className={styles.page}>
+        <div className="page__container">
+          <div className={styles.page__body}></div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
