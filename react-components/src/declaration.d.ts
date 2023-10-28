357b77f32db951a1c655a3025ee6f38ca0a98c01
FreightDH <maxim.zolotkov@yandex.ru>
2023-10-28 13:00:25 +0300

    feat: add data and pokemon data into global types

--- react-components/src/declaration.d.ts
+++ react-components/src/declaration.d.ts
@@ -2,3 +2,27 @@ declare module '*.module.scss' {
   const styles: { [key: string]: string };
   export default styles;
 }
+
+type Data = {
+  count: number;
+  next: string | null;
+  prev: string | null;
+  results: Array<Record<string, string>>;
+} | null;
+
+type PokemonData = {
+  id: number;
+  stats: [
+    { base_stat: number },
+    { base_stat: number },
+    { base_stat: number },
+    { base_stat: number },
+    { base_stat: number },
+    { base_stat: number },
+  ];
+  name: string;
+  sprites: {
+    other: { dream_world: { front_default: string } };
+  };
+  types: [{ type: { name: string } }];
+} | null;
