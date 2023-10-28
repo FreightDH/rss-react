d6b5272d086dbc8fa532e367b6df48651caa4f82
FreightDH <maxim.zolotkov@yandex.ru>
2023-10-28 13:01:10 +0300

    feat: add initial request with all pokemons

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,17 +1,35 @@
 import { Component } from 'react';
 
 import SearchSection from './components/InputSection/SearchSection';
+import ListSection from './components/ListSection/ListSection';
 
 import styles from './App.module.scss';
 
 const URL = 'https://pokeapi.co/api/v2/pokemon/';
 
 interface AppProps {}
-interface AppState {}
+interface AppState {
+  data: Data;
+  isLoading: boolean;
+}
 
 class App extends Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props);
+
+    this.state = {
+      data: null,
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ data: data, isLoading: false });
+      })
+      .catch((error) => console.error(error));
   }
 
   getPokemonData = (pokemonName: string) => {
@@ -20,7 +38,7 @@ class App extends Component<AppProps, AppState> {
     fetch(finalURL)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        this.setState({ ...this.state, data: data });
       })
       .catch((error) => console.error(error));
   };
@@ -31,9 +49,10 @@ class App extends Component<AppProps, AppState> {
         <main className={styles.page}>
           <div className="page__container">
             <div className={styles.page__body}>
-              <h1 className={styles.page__title}>Pokemon Cards</h1>
+              <h1 className={styles.page__title}>Pokemon Card</h1>
               <SearchSection getPokemonData={this.getPokemonData} />
               <div style={{ width: '100%', height: '1px', backgroundColor: '#000' }}></div>
+              {this.state.isLoading ? <p>Loading data...</p> : <ListSection data={this.state.data} />}
             </div>
           </div>
         </main>
