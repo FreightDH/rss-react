6f71d11d2651d438e55fa700b157fbd41f951ef7
FreightDH <maxim.zolotkov@yandex.ru>
2023-11-01 16:11:31 +0300

    refactor: change footer to functional component

--- react-components/src/components/Footer/Footer.tsx
+++ react-components/src/components/Footer/Footer.tsx
@@ -1,30 +1,24 @@
-import { Component } from 'react';
+import { ReactElement } from 'react';
 import styles from './Footer.module.scss';
 
-class Footer extends Component {
-  copyrightText: string;
+const Footer = (): ReactElement => {
+  const copyrightText = ' © 2023 All Rights Reserved | ';
 
-  constructor(props = {}) {
-    super(props);
-    this.copyrightText = ' © 2023 All Rights Reserved | ';
-  }
-  render() {
-    return (
-      <footer className={styles.footer}>
-        <div className="footer__container">
-          <p className={styles.footer__copyright}>
-            <a href="https://github.com/FreightDH" target="blank" rel="noreferrer">
-              FreightDH
-            </a>
-            {this.copyrightText}
-            <a href="https://rs.school/react/" target="blank" rel="noreferrer">
-              RSSchool
-            </a>
-          </p>
-        </div>
-      </footer>
-    );
-  }
-}
+  return (
+    <footer className={styles.footer}>
+      <div className="footer__container">
+        <p className={styles.footer__copyright}>
+          <a href="https://github.com/FreightDH" target="blank" rel="noreferrer">
+            FreightDH
+          </a>
+          {copyrightText}
+          <a href="https://rs.school/react/" target="blank" rel="noreferrer">
+            RSSchool
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
