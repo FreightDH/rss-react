1bfe6b21cc92f837fabf3d88e1c78f07391439a0
FreightDH <maxim.zolotkov@yandex.ru>
2023-11-01 21:21:40 +0300

    refactor: change list section to functional component

--- react-components/src/components/ListSection/ListSection.tsx
+++ react-components/src/components/ListSection/ListSection.tsx
@@ -1,4 +1,4 @@
-import { Component, ReactNode } from 'react';
+import { ReactElement, ReactNode } from 'react';
 import PokemonCard from './PokemonCard/PokemonCard';
 import styles from './ListSection.module.scss';
 
@@ -7,28 +7,20 @@ interface ListSectionProps {
   pokemonDataURL: string;
 }
 
-class ListSection extends Component<ListSectionProps> {
-  constructor(props: ListSectionProps) {
-    super(props);
+const ListSection: React.FC<ListSectionProps> = ({ data, pokemonDataURL }): ReactElement => {
+  let listBody: ReactNode | ReactNode[];
+
+  if (pokemonDataURL) {
+    listBody = <PokemonCard requestURL={pokemonDataURL} />;
+  } else if (data) {
+    listBody = data.results.map((item) => <PokemonCard requestURL={item.url} key={item.name} />);
   }
 
-  render() {
-    let listBody: ReactNode | ReactNode[];
-
-    if (this.props.pokemonDataURL) {
-      listBody = <PokemonCard requestURL={this.props.pokemonDataURL} />;
-    } else if (this.props.data) {
-      listBody = this.props.data.results.map((item) => <PokemonCard requestURL={item.url} key={item.name} />);
-    } else {
-      listBody = <li>No data</li>;
-    }
-
-    return (
-      <section className={styles.list}>
-        <ul className={styles.list__body}>{listBody}</ul>
-      </section>
-    );
-  }
-}
+  return (
+    <section className={styles.list}>
+      <ul className={styles.list__body}>{listBody}</ul>
+    </section>
+  );
+};
 
 export default ListSection;
--- react-components/src/components/ListSection/PokemonCard/PokemonCard.tsx
+++ react-components/src/components/ListSection/PokemonCard/PokemonCard.tsx
@@ -1,4 +1,4 @@
-import { Component, ReactNode } from 'react';
+import { ReactElement, useCallback, useEffect, useState } from 'react';
 import { RotatingLines } from 'react-loader-spinner';
 import cardsColors from './cardsColors';
 import styles from './PokemonCard.module.scss';
@@ -7,106 +7,83 @@ interface PokemonCardProps {
   requestURL: string;
 }
 
-interface PokemonCardState {
-  pokemonData: PokemonData | null;
-  isLoading: boolean;
-  badRequest: boolean;
-}
+const PokemonCard: React.FC<PokemonCardProps> = ({ requestURL }): ReactElement => {
+  const [pokemonData, setPokemonData] = useState<PokemonData | null>(null);
+  const [statuses, setStatuses] = useState({ isLoading: false, isBadRequest: false });
 
-class PokemonCard extends Component<PokemonCardProps, PokemonCardState> {
-  constructor(props: PokemonCardProps) {
-    super(props);
-
-    this.state = {
-      pokemonData: null,
-      isLoading: true,
-      badRequest: false,
-    };
-  }
-
-  componentDidMount() {
-    this.getPokemonData();
-  }
-
-  componentDidUpdate(prevProps: Readonly<PokemonCardProps>): void {
-    if (this.props.requestURL !== prevProps.requestURL) {
-      this.getPokemonData();
+  const fetchPokemonData = useCallback(async () => {
+    try {
+      setStatuses({ isBadRequest: false, isLoading: true });
+      const res = await fetch(requestURL);
+      const data: PokemonData = await res.json();
+      setPokemonData(data);
+      setStatuses({ isBadRequest: false, isLoading: false });
+    } catch (error) {
+      setStatuses({ isLoading: false, isBadRequest: true });
+      throw new Error('Invalid pokemon name!');
     }
+  }, [requestURL]);
+
+  useEffect(() => {
+    fetchPokemonData();
+  }, [fetchPokemonData]);
+
+  if (statuses.isLoading) {
+    return <RotatingLines width="40" strokeColor="#353535" strokeWidth="5" animationDuration="0.75" visible={true} />;
   }
 
-  getPokemonData() {
-    this.setState({ ...this.state, isLoading: true });
-    fetch(this.props.requestURL)
-      .then((res) => res.json())
-      .then((data) => {
-        this.setState({ pokemonData: data, isLoading: false, badRequest: false });
-      })
-      .catch(() => {
-        this.setState({ ...this.state, badRequest: true });
-        throw new Error("Bad request, pokemon name wasn't found!");
-      });
+  if (statuses.isBadRequest) {
+    return <p>{"Pokemon wasn't found! Please, enter the other name."}</p>;
   }
 
-  render() {
-    let componentBody: ReactNode;
-
-    if (this.state.badRequest) {
-      componentBody = <p>{"Pokemon wasn't found! Please, enter the other name."}</p>;
-    } else if (this.state.isLoading) {
-      componentBody = (
-        <RotatingLines width="40" strokeColor="#353535" strokeWidth="5" animationDuration="0.75" visible={true} />
-      );
-    } else {
-      componentBody = (
-        <li
-          className={styles.card}
-          style={{
-            background: `radial-gradient(circle at 50% 0%, ${
-              cardsColors[this.state.pokemonData!.types[0].type.name]
-            } 36%, #ffffff 36%)`,
-          }}
-        >
-          <p className={styles.card__health}>
-            <span>HP</span>
-            {`'${this.state.pokemonData!.stats[0].base_stat}'`}
-          </p>
-          <div className={styles.card__image}>
-            <img src={this.state.pokemonData!.sprites.other.dream_world.front_default} alt="pokemon" />
-          </div>
-          <h2 className={styles.card__name}>
-            {this.state.pokemonData!.name[0].toUpperCase() + this.state.pokemonData!.name.slice(1)}
-          </h2>
-          <ul className={styles.card__types}>
-            {this.state.pokemonData!.types.map((item, index) => (
-              <li
-                className={styles.types__item}
-                style={{ backgroundColor: `${cardsColors[this.state.pokemonData!.types[index].type.name]}` }}
-                key={index}
-              >
-                {item.type.name}
-              </li>
-            ))}
-          </ul>
-          <ul className={styles.card__stats}>
-            <li className={styles.stats__item}>
-              <h3 className={styles.item__stat}>{this.state.pokemonData!.stats[1].base_stat}</h3>
-              <p className={styles.item__name}>Attack</p>
+  if (pokemonData !== null) {
+    return (
+      <li
+        className={styles.card}
+        style={{
+          background: `radial-gradient(circle at 50% 0%, ${
+            cardsColors[pokemonData!.types[0].type.name]
+          } 36%, #ffffff 36%)`,
+        }}
+      >
+        <p className={styles.card__health}>
+          <span>HP</span>
+          {`'${pokemonData!.stats[0].base_stat}'`}
+        </p>
+        <div className={styles.card__image}>
+          <img src={pokemonData!.sprites.other.dream_world.front_default} alt="pokemon" />
+        </div>
+        <h2 className={styles.card__name}>{pokemonData!.name[0].toUpperCase() + pokemonData!.name.slice(1)}</h2>
+        <ul className={styles.card__types}>
+          {pokemonData!.types.map((item, index) => (
+            <li
+              className={styles.types__item}
+              style={{ backgroundColor: `${cardsColors[pokemonData!.types[index].type.name]}` }}
+              key={index}
+            >
+              {item.type.name}
             </li>
-            <li className={styles.stats__item}>
-              <h3 className={styles.item__stat}>{this.state.pokemonData!.stats[2].base_stat}</h3>
-              <p className={styles.item__name}>Defense</p>
-            </li>
-            <li className={styles.stats__item}>
-              <h3 className={styles.item__stat}>{this.state.pokemonData!.stats[5].base_stat}</h3>
-              <p className={styles.item__name}>Speed</p>
-            </li>
-          </ul>
-        </li>
-      );
-    }
-
-    return <>{componentBody}</>;
+          ))}
+        </ul>
+        <ul className={styles.card__stats}>
+          <li className={styles.stats__item}>
+            <h3 className={styles.item__stat}>{pokemonData!.stats[1].base_stat}</h3>
+            <p className={styles.item__name}>Attack</p>
+          </li>
+          <li className={styles.stats__item}>
+            <h3 className={styles.item__stat}>{pokemonData!.stats[2].base_stat}</h3>
+            <p className={styles.item__name}>Defense</p>
+          </li>
+          <li className={styles.stats__item}>
+            <h3 className={styles.item__stat}>{pokemonData!.stats[5].base_stat}</h3>
+            <p className={styles.item__name}>Speed</p>
+          </li>
+        </ul>
+      </li>
+    );
   }
-}
+
+  return <></>;
+};
 
 export default PokemonCard;
