b2420c37afe6d1b16d10501db119cdcfcf9e3535
FreightDH <maxim.zolotkov@yandex.ru>
2023-11-05 15:06:18 +0300

    refactor: list section

--- react-routing/src/components/ListSection/ListSection.tsx
+++ react-routing/src/components/ListSection/ListSection.tsx
@@ -1,23 +1,25 @@
-import { FC, ReactElement, ReactNode } from 'react';
+import { CSSProperties, FC, ReactElement, ReactNode } from 'react';
 import PokemonCard from './PokemonCard/PokemonCard';
 import styles from './ListSection.module.scss';
 
 interface ListSectionProps {
   data: Data | null;
-  pokemonDataURL: string;
+  pokemonURL: string;
 }
 
-const ListSection: FC<ListSectionProps> = ({ data, pokemonDataURL }): ReactElement => {
+const ListSection: FC<ListSectionProps> = ({ data, pokemonURL }): ReactElement => {
   let listBody: ReactNode | ReactNode[];
+  let listPadding: CSSProperties = {};
 
-  if (pokemonDataURL) {
-    listBody = <PokemonCard requestURL={pokemonDataURL} />;
+  if (pokemonURL) {
+    listBody = <PokemonCard requestURL={pokemonURL} />;
+    listPadding = { paddingTop: '20px' };
   } else if (data) {
     listBody = data.results.map((item) => <PokemonCard requestURL={item.url} key={item.name} />);
   }
 
   return (
-    <section className={styles.list}>
+    <section className={styles.list} style={listPadding}>
       <ul className={styles.list__body}>{listBody}</ul>
     </section>
   );
